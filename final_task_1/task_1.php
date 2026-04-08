bbdf65e62e6fc38bdfc1c6dc38a94603adde981d
ar-sohan <arsohan101@gmail.com>
2026-04-08 17:04:28 +0600

    first php code

---
+++ final_task_1/task_1.php
@@ -0,0 +1,11 @@
+<?php
+
+    $width = 20;
+    $height = 20;
+
+    $area = $width * $height;
+    echo "Area : {$area} <br>";
+
+    $perimeter = 2 * ($width + $height);
+    echo "Perimeter : {$perimeter}";
+?>
