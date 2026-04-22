d76bd833ce27bd94af69fb956607f8c39f8cbe4d
ar-sohan <arsohan101@gmail.com>
2026-04-22 16:51:56 +0600

    final lab 3 : first commit

---
+++ final_task_3/home.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <title> Unknown Company </title>
+</head>
+<body>
+    <header>
+        <div style="display: flex; justify-content: space-between;border: 2px solid black; padding:20px">
+            <div>
+                <h1>  Unknown Company  </h1> 
+            </div>
+            <div>
+                <ul style="display: flex; gap: 30px; list-style-type: none;">
+                    <li><a>Home</a></li>
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="registration.php">Register</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <main style = "border: 2px solid black; padding:20px; height: 350px;">
+        <p style="text-align : center;">Welcome to unknown company</p>
+    </main>
+    <footer style = "border: 2px solid black; padding:20px">
+        <p style="text-align : center;">copyright @2026</p>
+    </footer>
+</body>
+</html>
